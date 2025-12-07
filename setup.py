b5c86d22babe8df995aaf4f14150bc430492d5a6
Jomo Fisher <jomofisher@gmail.com>
2025-12-07 23:59:09 +0000

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="srsdb",
-    version="0.2.0",
+    version="0.3.0",
     author="jomof",
     author_email="",
     description="SQLite database for tracking SRS learning state with FSRS and Ebisu algorithms",
